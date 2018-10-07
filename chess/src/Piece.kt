7693cfcdee30013801ba5aee02717401a1f8308d
Areman Hashemi <aremanhashemi@gmail.com>
2018-10-07 12:28:53 -0700

    commit

--- chess/src/Piece.kt
+++ chess/src/Piece.kt
@@ -1,8 +1,57 @@
-interface Piece
+import java.util.*
 
-data class King (val color: Boolean) : Piece
-data class Queen (val color : Boolean) : Piece
-data class Knight (val color : Boolean): Piece
-data class Rook (val color : Boolean): Piece
-data class Bishop (val color : Boolean): Piece
-data class Pawn (val color : Boolean): Piece
+abstract class Piece
+
+data class King (val color: Color) : Piece()
+data class Queen (val color : Color) : Piece()
+data class Knight (val color : Color): Piece()
+data class Rook (val color : Color): Piece()
+data class Bishop (val color : Color): Piece()
+data class Pawn (val color: Color): Piece()
+
+enum class Color {
+    White,
+    Black
+}
+
+
+typealias Grid = Array<Array<Piece?>>
+class Board {
+
+    private val grid: Grid = createBoard()
+
+    private fun createBoard(): Grid = arrayOf(
+            createBaseRow(Color.White),
+            createPawnRow(Color.White),
+            createEmptyRow(),
+            createEmptyRow(),
+            createEmptyRow(),
+            createEmptyRow(),
+            createPawnRow(Color.Black),
+            createBaseRow(Color.Black))
+
+    private fun createEmptyRow(): Array<Piece?> = arrayOfNulls(8)
+    private fun createBaseRow(color: Color): Array<Piece?> =
+            arrayOf(
+                    Rook(color),
+                    Knight(color),
+                    Bishop(color),
+                    Queen(color),
+                    King(color),
+                    Bishop(color),
+                    Knight(color),
+                    Rook(color))
+    private fun createPawnRow(color: Color): Array<Piece?> {
+        val row = arrayOfNulls<Piece?>(8)
+        for (i in 0..7) {
+            row[i] = Pawn(color)
+        }
+        return row
+    }
+
+    override fun toString(): String = Arrays.toString(grid)
+}
+
+fun main(args: Array<String>) {
+    println(Board())
+}
